12ef67dcf1bd4f3f14724782f211514c30772579
Javier Lopez <javilc@gmail.com>
2013-07-10 13:44:17 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,8 +3,10 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync('index.html');
+buf.toString('utf8',1,5);
 app.get('/', function(request, response) {
-  response.send('prueba');
+  response.send('prueba'+buf.toString('utf8',1,5));
 });
 
 var port = process.env.PORT || 5000;
